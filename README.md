037944b6736e54dd2ab12fc0df26408da1f7f1a5
Junaid <zulfiqar.junaid95@gmail.com>
2022-06-24 23:40:54 -0700

    for_deployment

---
+++ README.md
@@ -0,0 +1,7 @@
+# Netflix Clone ReactJS
+## Demo
+
+
+https://user-images.githubusercontent.com/101246922/160498198-eb082c5d-d7a3-4a7d-8c22-21bdf3dbb6cc.mov
+
+
---
+++ src/Nav.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from "react";
+import "./Nav.css";
+function Nav() {
+  const [navbarBlack, setNavbarBlack] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      setNavbarBlack(true);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+  return (
+    <nav className={`${navbarBlack && "nav__black"}`}>
+      <div className="nav__contents">
+        <img
+          className="nav__logo"
+          src="https://pngimg.com/uploads/netflix/netflix_PNG32.png"
+        />
+      </div>
+    </nav>
+  );
+}
+
+export default Nav;
